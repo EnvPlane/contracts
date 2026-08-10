ebf139f716fbc03caaf5be1d1b2d0eeea84d50f1
Alex <alex@Mac.fritz.box>
2026-08-10 15:13:40 +0200

    feat: add tenant-scoped saml contracts

---
+++ domain/saml.go
@@ -0,0 +1,25 @@
+package domain
+
+import "time"
+
+type SAMLProviderConfig struct {
+	TenantID         string            `json:"tenantId"`
+	ProviderID       string            `json:"providerId"`
+	Issuer           string            `json:"issuer"`
+	Audience         string            `json:"audience"`
+	MetadataRef      SecretReference   `json:"metadataRef"`
+	SigningKeyRef    SecretReference   `json:"signingKeyRef"`
+	AttributeMapping map[string]string `json:"attributeMapping"`
+	Enabled          bool              `json:"enabled"`
+}
+
+type SAMLAssertion struct {
+	AssertionID    string            `json:"assertionId"`
+	Issuer         string            `json:"issuer"`
+	Audience       string            `json:"audience"`
+	Subject        string            `json:"subject"`
+	Attributes     map[string]string `json:"attributes"`
+	NotBefore      time.Time         `json:"notBefore"`
+	NotOnOrAfter   time.Time         `json:"notOnOrAfter"`
+	SignatureValid bool              `json:"signatureValid"`
+}
